2850abead8adb32e894c3987b03b835402f17aeb
mirza <rajwan.mirza@gmail.com>
2018-02-12 02:49:07 +0600

    first commit
    Test app

---
+++ MiraclePill/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MiraclePill
+//
+//  Created by Mirza Rajwan on 12/2/18.
+//  Copyright © 2018 Mirza Rajwan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
